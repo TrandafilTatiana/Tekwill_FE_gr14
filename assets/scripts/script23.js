f3ce479e53105b2961f33e435cfc08d9fb42a41d
TrandafilTatiana <trandafiltatiana@gmail.com>
2025-02-09 11:07:49 +0200

    Switch

--- assets/scripts/script23.js
+++ assets/scripts/script23.js
@@ -19,3 +19,75 @@ let isLeapYear = (currentYear % 4 === 0 && currentYear % 100 !== 0) || (currentY
 let message = isLeapYear ? 'Год високосный' : 'Год не високосный';
 console.log(message);
 
+let countyCode = 'ES';
+let countryName;
+switch (countyCode) {
+  case 'MD':
+    countryName = 'Moldova';
+    break;
+  case 'RO':
+    countryName = 'Romania';
+    break;
+  case 'ES':
+    countryName = 'Spain';
+    break;
+  case 'US':
+    countryName = 'United States';
+    break;
+  case 'GR':
+    countryName = 'Greece';
+    break;
+  case 'BG':
+    countryName = 'Bulgaria';
+    break;
+  default:
+    countryName = 'Unknown Country';
+}
+console.log(countryName);
+
+let point = 79;
+let mark;
+switch (true) {
+  case point >= 97:
+    mark = "A+";
+    break;
+  case point >= 93:
+    mark = "A";
+    break;
+  case point >= 90:
+    mark = "A-";
+    break;
+  case point >= 87:
+    mark = "B+";
+    break;
+  case point >= 83:
+    mark = "B";
+    break;
+  case point >= 80:
+    mark = "B-";
+    break;
+  case point >= 77:
+    mark = "C+";
+    break;
+  case point >= 73:
+    mark = "C";
+    break;
+  case point >= 70:
+    mark = "C-";
+    break;
+  case point >= 67:
+    mark = "D+";
+    break;
+  case point >= 63:
+    mark = "D";
+    break;
+  case point >= 60:
+    mark = "D-";
+    break;
+  case point >= 0:
+    mark = "F";
+    break;
+  default:
+    mark = "Invalid score";
+}
+console.log(mark);
